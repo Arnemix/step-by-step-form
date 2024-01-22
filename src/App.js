da8c9cefbf2e5952bceae28c976acdcc2b8c4281
Arnemix <amindrayef@gmail.com>
2024-01-22 12:57:51 +0100

    Début sidebar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
-import React from 'react';
-import { useState } from 'react';
+import React from "react";
+import { useState } from "react";
+import "./App.scss";
 function App() {
-    return (
-        <div className='App'>
+    let STEPS = {
+        STEP_1: "Your info",
+        STEP_2: "Select plan",
+        STEP_3: "Add-ons",
+        STEP_4: "Summary",
+    };
 
+    return (
+        <div className="App">
             <div className="form">
                 <div className="form-sidebar">
-                    
+                    {
+                        Object.keys(STEPS).map((key, index) => {
+                            let className = "sidebar-step" + key;
+                            return (
+                                <div className={className}>
+                                    <div className="sidebar-step-number">
+                                        {index + 1}
+                                    </div>
+                                    <div>
+                                        <span>step {index + 1}</span>
+                                        <p>{STEPS[key]}</p>
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
                 </div>
             </div>
-
         </div>
     );
 }
 
 export default App;
-
