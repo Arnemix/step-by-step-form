fb8c9d11d04de37f00c12a0956e0127e63eeaf9b
Arnemix <amindrayef@gmail.com>
2024-01-22 13:14:19 +0100

    Avancement sidebar

--- src/App.js
+++ src/App.js
@@ -13,22 +13,18 @@ function App() {
         <div className="App">
             <div className="form">
                 <div className="form-sidebar">
-                    {
-                        Object.keys(STEPS).map((key, index) => {
-                            let className = "sidebar-step" + key;
-                            return (
-                                <div className={className}>
-                                    <div className="sidebar-step-number">
-                                        {index + 1}
-                                    </div>
-                                    <div>
-                                        <span>step {index + 1}</span>
-                                        <p>{STEPS[key]}</p>
-                                    </div>
+                    {Object.keys(STEPS).map((key, index) => {
+                        let className = "sidebar-step " + key;
+                        return (
+                            <div className={className}>
+                                <div className="sidebar-step-number">{index + 1}</div>
+                                <div>
+                                    <span className="sidebar-step-text">step {index + 1}</span>
+                                    <p>{STEPS[key]}</p>
                                 </div>
-                            )
-                        })
-                    }
+                            </div>
+                        );
+                    })}
                 </div>
             </div>
         </div>
