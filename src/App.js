1f887fc1edd5952dd4bc1ed04fc5cb4c0f41bb18
Arnemix <amindrayef@gmail.com>
2024-01-22 12:38:49 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useState } from 'react';
+function App() {
+    return (
+        <div className='App'>
+
+            <div className="form">
+                <div className="form-sidebar">
+                    
+                </div>
+            </div>
+
+        </div>
+    );
+}
+
+export default App;
+
